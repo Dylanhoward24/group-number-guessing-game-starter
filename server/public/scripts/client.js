082099ee0b08c009fccd194a5b9e9eec51709749
Dylan Howard <dylanhoward0424@outlook.com>
2021-07-29 13:46:28 -0500

    beatiful table stuff

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,11 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+
+  $.ajax({
+    method: "GET",
+    url: "/guesses"
+  }).then((response) => {
+    
+  })
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+let storedGuesses = [];
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -10,7 +11,16 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
+let target = getRandomNumber(1, 25);
+
+app.get('/guesses', (req, res) => {
+  let newGuesses = req.body;
+  console.log(newGuesses);
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
